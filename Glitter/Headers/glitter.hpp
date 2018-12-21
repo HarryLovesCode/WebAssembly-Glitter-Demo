e220a96873ec51b81cfcde501ba25dc3f3c47294
Harry Gould <Harrison.Gould16@Gmail.com>
2018-12-20 19:42:00 -0500

    Didn't need bullet

--- Glitter/Headers/glitter.hpp
+++ Glitter/Headers/glitter.hpp
@@ -7,7 +7,6 @@
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
 #include <assimp/scene.h>
-#include <btBulletDynamicsCommon.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <stb_image.h>
